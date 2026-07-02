be1d2ac16f65465cb060f7c81d1f3c97043b6693
danduthanishkareddy <thanishkareddy777@gmail.com>
2026-07-02 20:12:38 +0530

    Add files via upload

---
+++ Source Code/Services/fact_checker.py
@@ -0,0 +1,17 @@
+import wikipedia
+
+
+def fact_check(topic):
+    try:
+        summary = wikipedia.summary(topic, sentences=2)
+
+        return {
+            "topic": topic,
+            "summary": summary
+        }
+
+    except Exception:
+        return {
+            "topic": topic,
+            "summary": "No information found."
+        }
